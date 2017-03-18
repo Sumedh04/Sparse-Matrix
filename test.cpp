32bdbfc47b55a0a8193fa870dcb02d35821d0958
Sumedh04 <sumedhm14@gmail.com>
2017-03-19 00:06:24 +0530

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,19 +1,19 @@
-#include <Rcpp.h> 								// Include the required header file for functions like Rcout
-using namespace Rcpp; 							// Namespace for R functions in c++
+#include <Rcpp.h> 	// Include the required header file for functions like Rcout
+using namespace Rcpp; 	// Namespace for R functions in c++
 
 /*
 This cpp function can be called from an R console by typing the following commands. 
 
-> Rcpp::sourceCpp("test.cpp") 					// Properly type-in the file location. The "test.cpp" file will be sourced in the current R session.
-> print_matrix_cpp(x)							// Call the function
+> Rcpp::sourceCpp("test.cpp") // Properly type-in the file location. The "test.cpp" file will be sourced in the current R session.
+> print_matrix_cpp(x)	      // Call the function
 
 */
 
-void print_matrix_cpp(S4 object)				// Definition of function starts here.
-												// A properly defined S4 object is input argument to the function and return type is void.
+void print_matrix_cpp(S4 object)  // Definition of function starts here.
+// A properly defined S4 object is input argument to the function and return type is void.
 {
 
-	// "slot" function is called for the S4 object with respective string input arguments to get the type, dimension and other parameters of the object.
+// "slot" function is called for the S4 object with respective string input arguments to get the type, dimension and other parameters of the object.
 	
 	CharacterVector matrix_type = object.slot("class");
 	NumericVector dimension = obj.slot("Dim");
